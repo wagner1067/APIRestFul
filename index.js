8f126c9a094d66f28a82bfc2f30823186c4d871a
wagner1067 <barra-wag@hotmail.com>
2025-05-29 21:10:38 -0300

    feat: Metod Get

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import http from "http";
 
 const port = 3000;
+const grades = [
+  {
+    studentName: "Wagner",
+    subject: "Matemática",
+    grade: 8.5,
+  },
+];
 
 const server = http.createServer((req, res) => {
   //Função do backend
+  const { method } = req;
+  const { url } = req;
+  if (url === "/grades" && method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(grades));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Rota não encontrada" }));
+  }
 });
 
 server.listen(port, () => {
